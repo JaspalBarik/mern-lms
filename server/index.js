18b257c8e16bc3a6a6916176b0fb305ab5c717e5
JASPAL BARIK <barikjaspal2001@gmail.com>
2025-09-10 12:31:42 +0530

    Fix CORS for frontend deployment

--- server/index.js
+++ server/index.js
@@ -21,7 +21,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // changed
+    origin: [
+    "http://localhost:5173", // for local dev
+     process.env.FRONTEND_URL, // changed
+    ],
     credentials: true,
 }));
 
